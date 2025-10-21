1615dd48dd52c86fcb41f72e1511a234360cd63e
danethomas8 <daneislive@gmail.com>
2025-10-22 03:16:42 +0530

    Initial commit: Java program

---
+++ src/main/java/practiceJava/coreJavaCodingQuestions.java
@@ -0,0 +1,22 @@
+package practiceJava;
+
+public class coreJavaCodingQuestions {
+
+	public static void main(String[] args) {
+		
+		try {
+			int i = 10/0;
+			
+		}catch(ArithmeticException e) {
+			System.out.println("Error: Division by zero");
+			e.printStackTrace();
+			
+		}finally {
+			System.out.println("This block will always execute");
+		}
+		
+		
+
+	}
+
+}
