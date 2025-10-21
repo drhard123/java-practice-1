3750e245b0519f1a211d98980c8b7513b6accdf0
drhard123 <danethomas143@gmail.com>
2025-10-22 05:12:25 +0530

    Added string reversal code

--- src/main/java/practiceJava/coreJavaCodingQuestions.java
+++ src/main/java/practiceJava/coreJavaCodingQuestions.java
@@ -4,6 +4,9 @@ public class coreJavaCodingQuestions {
 
 	public static void main(String[] args) {
 		
+		
+		/* 1. Example for exception handling */
+		
 		try {
 			int i = 10/0;
 			
@@ -14,6 +17,17 @@ public class coreJavaCodingQuestions {
 		}finally {
 			System.out.println("This block will always execute.");
 		}
+		
+		
+		/* 2. Example for reversing a string */
+		
+		String str1 = "John Doe";
+		String reverse = "";
+		for (int i = str1.length()-1; i>=0; i--) {
+			reverse += str1.charAt(i);
+		}
+		
+		System.out.println("Reverse of the string "+str1+" = "+reverse);
 		
 		
 
